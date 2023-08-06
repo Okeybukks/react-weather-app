d6e8fcafce25e6611561798c9650c808599ad51d
Okeybukks <achebepeter94@gmail.com>
2023-08-07 00:27:39 +0100

    test

--- shared-library/vars/helloWorld.groovy
+++ shared-library/vars/helloWorld.groovy
@@ -1,15 +1,3 @@
-// def call(Map name = [:]) {
-//     return "Hello ${name.firstName} ${name.lastName}"
-// }
-
-def call(String gitBranch) {
-    def envi = ''
-    if(gitBranch =~ '^feat' || gitBranch =~ '^dev'){
-        envi = 'dev'
-    }
-    else if(gitBranch =~ '^mas') {
-        envi = 'prod'
-    }
-
-    return envi
+def call(Map name = [:]) {
+    return "Hello ${name.firstName} ${name.lastName}"
 }
